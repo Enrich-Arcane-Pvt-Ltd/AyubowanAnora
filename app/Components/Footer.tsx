c23b4c2524f0f0d6b86acd6b59e3e8786719ff77
savithyacomester <savithya.enrich@gmail.com>
2025-11-12 12:06:02 +0530

    Ayubowan Anora (Dev-Savithya)

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+const year = new Date().getFullYear();
 const Footer = () => (
   <footer className="bg-purple-700 text-white text-center py-6">
-    <p>© 2026 AYUBOWAN–ANORA | Organized by Anora Entertainment UAE & Sri Lanka</p>
+    <p>© {year} AYUBOWAN–ANORA | Organized by Anora Entertainment UAE & Sri Lanka</p>
   </footer>
 );
 
