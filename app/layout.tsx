f017cc96b251e70cd32538c8ced09a9c010e18ba
Rishen-Tharusha <rishen.enrich@gmail.com>
2025-11-12 10:15:28 +0530

    Changes - Dev Rishen

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ export const metadata = {
   description: "Coming soon page for Ayubowan Anora",
 };
 
+// Comment
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
